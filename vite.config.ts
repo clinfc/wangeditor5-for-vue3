d711739dfe781b089bb15bd494faaa680d6afa35
clinfc <1962109568@qq.com>
2022-02-18 18:36:25 +0800

    chore: build config

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  mode: 'production',
+  server: {
+    host: '0.0.0.0',
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+    target: 'es6',
+    outDir: 'dist/lib',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['umd'],
+      name: 'wangEditor5ForVue3',
+      fileName: () => 'index.js',
+    },
+    rollupOptions: {
+      external: ['vue', '@wangeditor/editor'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          '@wangeditor/editor': 'wangEditor',
+        },
+      },
+    },
+  },
+})
