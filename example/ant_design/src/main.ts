7453c7800e14f0732660eec8876c908f39e7e20f
clinfc <1962109568@qq.com>
2022-04-17 17:21:23 +0800

    chore: toggle-mode 示例

--- example/ant_design/src/main.ts
+++ example/ant_design/src/main.ts
@@ -3,8 +3,17 @@ import router from '@router/router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
-import { weEditorPlusCssRule, WeEditable, WeEditor, WeEditorPlus, WeToolbar } from 'wangeditor5-for-vue3'
+import {
+  weEditorPlusCssRule,
+  WeEditable,
+  WeEditor,
+  WeEditorPlus,
+  WeToolbar,
+  registToggleMode,
+} from 'wangeditor5-for-vue3'
 import weFormFields from './plugins/form-fields'
+
+registToggleMode()
 
 // 将 wangEditor 的 css 引入为 纯文本/路径
 import cssstyle from '@wangeditor/editor/dist/css/style.css'
--- example/vue_cli/src/main.ts
+++ example/vue_cli/src/main.ts
@@ -2,9 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { weEditorPlusCssRule, WeToolbar, WeEditable, WeEditor, WeEditorPlus } from 'wangeditor5-for-vue3'
+import {
+  weEditorPlusCssRule,
+  registToggleMode,
+  WeToolbar,
+  WeEditable,
+  WeEditor,
+  WeEditorPlus,
+} from 'wangeditor5-for-vue3'
 
 import '@wangeditor/editor/dist/css/style.css'
+
+registToggleMode()
 
 // 由于 WeEditorPlus 在 vue-cli 项目中无法正常使用，因此注释掉的部分可以忽略
 // 内联 loader：https://webpack.docschina.org/concepts/loaders/#inline
