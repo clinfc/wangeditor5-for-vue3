8103aff3355670d1eca1fa25cf0e60161dba77af
clinfc <1962109568@qq.com>
2022-05-08 13:21:34 +0800

    chore: 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: 'production',
-  server: {
-    host: '0.0.0.0',
-    open: true,
-  },
   build: {
     sourcemap: true,
     target: 'esnext',
